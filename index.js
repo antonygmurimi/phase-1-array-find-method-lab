2d75dda80cf71dab2cff4a46d2c0700a4650d1a0
Antony <antonygmurimi@gmail.com>
2024-04-08 23:51:18 +0300

    find me array method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(array){
+    const win = array.find(function(record){
+      return record.result === "W";
+    })
+    if(win){
+      return win.year;
+    }
+    return undefined;
+  }
+// // const superbowlwin = [{year: 2015, result: "W"}]
+// const found = record.find(element => element.year == 2015);
+// console.log(found);
+// //   return undefined;
